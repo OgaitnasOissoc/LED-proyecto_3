ccba18e99444184bb693084a28bab12a9f132b11
diammenth <diammenth@diammenthmainpc.diammenth>
2026-04-28 12:45:12 -0600

    pandemic class done

--- logic.py
+++ logic.py
@@ -1,5 +1,27 @@
 import os
 import networkx as nx
 import matplotlib.pyplot as plt
-def lode_pandemic(a):
-    return nx.read_edgelist(a)
+from random import random
+
+class Pandemic:
+    def infect(self.n):
+        self.map.nodes[n]["status"] = "infected"
+
+
+    def __init__(self,location,starting):
+        self.map = nx.from_edgelist(location)
+        nx.set_node_attributes(self.map, {node: "healthy" for node in self.map.nodes}, "status")
+        self.infect(starting)
+
+    
+    def progress(self):
+        new_infected = []
+        for node in self.map.nodes:
+            if self.map.nodes[node]["status"] == "infected":
+                for i in self.map.neighbors(node):
+                    if self.map.nodes[i]["status"] == "healthy":
+                        if random() >= 0.5:
+                            new_infected.append(i)
+        for g in new_infected:
+            self.infect(g)
+
